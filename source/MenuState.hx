76f0b4fc1ea2e578daffd23c10b49727beec430c
Nathan Pinsker <npinsker@gmail.com>
2017-07-13 21:30:18 -0700

    Hello, world! text

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -12,6 +12,10 @@ class MenuState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		add(text);
 	}
 
 	override public function update(elapsed:Float):Void
