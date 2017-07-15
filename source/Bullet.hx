2181137a15524d43d983c585a02c6c9b7c423869
Nathan Pinsker <npinsker@gmail.com>
2017-07-14 21:44:52 -0700

    add bullet

---
+++ source/Bullet.hx
@@ -0,0 +1,21 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxColor;
+
+using flixel.util.FlxSpriteUtil;
+
+class Bullet extends FlxSpriteGroup {
+	private var bulletSprite:FlxSprite;
+    public function new(?X:Float=0, ?Y:Float=0) {
+        super(X, Y);
+		
+		bulletSprite = new FlxSprite();
+		bulletSprite.makeGraphic(10, 10, FlxColor.TRANSPARENT, true);
+		bulletSprite.x = bulletSprite.y = -5;
+		bulletSprite.drawCircle(5, 5, 5, FlxColor.YELLOW);
+		add(bulletSprite);
+    }
+}
