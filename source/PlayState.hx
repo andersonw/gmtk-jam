575cbcad16050d3bd686b6f3281506b7474e854e
Nathan Pinsker <npinsker@gmail.com>
2017-07-14 20:51:03 -0700

    move movement code

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.math.FlxPoint;
 import flixel.system.FlxSound;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
@@ -21,5 +22,58 @@ class PlayState extends FlxState {
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+		
+		handlePlayerMovement();
 	}
+	
+	function handlePlayerMovement():Void {
+        var _up:Bool = false;
+        var _down:Bool = false;
+        var _left:Bool = false;
+        var _right:Bool = false;
+		var speed;
+
+        _up = FlxG.keys.anyPressed([UP]);
+        _down = FlxG.keys.anyPressed([DOWN]);
+        _left = FlxG.keys.anyPressed([LEFT]);
+        _right = FlxG.keys.anyPressed([RIGHT]);
+
+        if (FlxG.keys.anyPressed([SHIFT]))
+            speed = 50;
+        else
+            speed = 200;
+
+        if (_up && _down)
+            _up = _down = false;
+        if (_left && _right)
+            _left = _right = false;
+
+        if (_up || _down || _left || _right) {
+            var mA:Float = 0;
+            if (_up) {
+                mA = -90;
+                if (_left) {
+                    mA -= 45;
+                } else if(_right) {
+                    mA += 45;
+                }
+            } else if (_down) {
+                mA = 90;
+                if (_left) {
+                    mA += 45;
+                } else if (_right) {
+                    mA -= 45;
+                }
+            } else if (_left) {
+                mA = 180;
+            } else if (_right) {
+                mA = 0;
+            }
+            _player.velocity.set(speed, 0);
+            _player.velocity.rotate(FlxPoint.weak(0,0), mA);
+        }
+        else {
+            _player.velocity.set(0, 0);
+        }
+    }
 }
