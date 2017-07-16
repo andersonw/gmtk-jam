ec4a4e0285e6cedeac04df914de3faa92c49af45
Nathan Pinsker <npinsker@gmail.com>
2017-07-16 00:13:17 -0700

    powerup pentagon remove

--- source/Powerup.hx
+++ source/Powerup.hx
@@ -1,11 +1,16 @@
 package;
 
+import flash.geom.ColorTransform;
+import flash.geom.Point;
+import flash.geom.Rectangle;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
 import flixel.util.FlxColor;
+import openfl.Assets;
+import openfl.display.BitmapData;
 
 using flixel.util.FlxSpriteUtil;
 
@@ -21,26 +26,52 @@ enum PowerupType {
 class Powerup extends FlxSpriteGroup {
 	private var _playState:PlayState;
     public var isInvincible:Bool;
+	private var SPRITE_WIDTH:Int = 40;
+	private var SPRITE_HEIGHT:Int = 40;
 
 	private static var kPowerupToColorMap:Map<PowerupType, FlxColor> =
 		[FIRE => FlxColor.RED, ICE => FlxColor.BLUE,
 		 LIGHTNING => FlxColor.YELLOW, METAL => FlxColor.GRAY,
 		 LUGE => FlxColor.WHITE];
 	 
-	private var powerupSprite:FlxSprite;
+	private var bombSprite:FlxSprite;
+	private var bombOutlineSprite:FlxSprite;
 	private var _type:PowerupType;
     public function new(?X:Float=0, ?Y:Float=0, type:PowerupType, ?playState:PlayState) {
         super(X, Y);
 		_type = type;
 		_playState = playState;
 		isInvincible = true;
-        haxe.Timer.delay(makeVulnerable.bind(),500);
-		powerupSprite = new FlxSprite();
-		powerupSprite.makeGraphic(20, 20, FlxColor.TRANSPARENT, true);
-		powerupSprite.x = powerupSprite.y = -10;
-		powerupSprite.drawPolygon([new FlxPoint(10, 0), new FlxPoint(0, 6), new FlxPoint(4, 20), new FlxPoint(16, 20), new FlxPoint(20, 6)],
-								  kPowerupToColorMap[type], { }, { } );
-		add(powerupSprite);
+        haxe.Timer.delay(makeVulnerable.bind(), 500);
+		
+		
+		var bombBitmapData:BitmapData = Assets.getBitmapData("assets/images/bomb.png");
+		
+		var bombInteriorBitmapData:BitmapData = new BitmapData(SPRITE_WIDTH, SPRITE_HEIGHT);
+		bombInteriorBitmapData.copyPixels(
+			bombBitmapData, new Rectangle(SPRITE_WIDTH, 0, SPRITE_WIDTH, SPRITE_HEIGHT), new Point(0, 0));
+		var desiredColor:FlxColor = Powerup.getColorOfType(type);
+		bombInteriorBitmapData.colorTransform(
+			bombInteriorBitmapData.rect,
+			new ColorTransform(1, 1, 1, 1,
+							   desiredColor.red * 7/10, desiredColor.green * 7/10, desiredColor.blue * 7/10));
+			
+		var bombOutlineBitmapData:BitmapData = new BitmapData(SPRITE_WIDTH, 40);
+		bombOutlineBitmapData.copyPixels(
+			bombBitmapData, new Rectangle(0, 0, SPRITE_WIDTH, SPRITE_HEIGHT), new Point(0, 0));
+			
+		bombSprite = new FlxSprite();
+		bombSprite.loadGraphic(bombInteriorBitmapData);
+		bombSprite.x = -SPRITE_WIDTH / 2;
+		bombSprite.y = -SPRITE_HEIGHT / 2;
+		
+		bombOutlineSprite = new FlxSprite();
+		bombOutlineSprite.loadGraphic(bombOutlineBitmapData);
+		bombOutlineSprite.x = -SPRITE_WIDTH / 2;
+		bombOutlineSprite.y = -SPRITE_HEIGHT / 2;
+		
+		add(bombSprite);
+		add(bombOutlineSprite);
     }
 	public function getType():PowerupType {
 		return _type;
@@ -51,7 +82,7 @@ class Powerup extends FlxSpriteGroup {
     }
 	
 	override public function getHitbox(?rect:FlxRect):FlxRect {
-		return powerupSprite.getHitbox();
+		return bombSprite.getHitbox();
 	}
 	
 	// static methods
