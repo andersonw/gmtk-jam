4425dd2454e87dc69eeb993cf825660acf6960fc
clue <colin.h.lu@gmail.com>
2017-07-16 20:02:38 -0500

    asdf

--- source/LevelHUD.hx
+++ source/LevelHUD.hx
@@ -86,7 +86,7 @@ class LevelHUD extends FlxSpriteGroup {
 		levelText.text = "Level: " + level;
 		levelText.size = 16;
         // BOSS LEVEL
-        if(level == (_gameState.boringEnemyCount.length - 1)) {
+        if(level == (Main.gameState.boringEnemyCount.length - 1)) {
             enemiesLeftText.text = "Defeat the Boss!";
             enemiesLeftText.size = 20;
         }
